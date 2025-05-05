5b2946e57ae1f08e5904d4ab22492adcd91169b2
AnnaBeatrizSilva2904 <annabeatryz2504@gmail.com>
2025-05-05 10:42:07 -0300

    Arrumado o código

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -9,6 +9,7 @@ import EmojiPicker from "@/components/EmojiPicker";
 import { type ImageSource } from 'expo-image'
 import EmojiList from "@/components/EmojiList";
 import EmojiSticker from "@/components/EmojiSticker";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -49,14 +50,15 @@ export default function Index() {
   }
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           imgSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
         {
-          pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+          pickedEmoji && 
+          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
         }
       </View>
       {showAppOptions ? (
@@ -83,7 +85,7 @@ export default function Index() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
